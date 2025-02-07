00ac3a3bf3f974a46d978552b133bb712b8a7c0e
Shokh-002 <reactjs2002@gmail.com>
2025-02-07 11:27:51 +0500

    final build

--- .next/server/app/api/type-indicator/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/type-indicator/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_60615f._.js");
-runtime.loadChunk("server/chunks/[root of the server]__4374b8._.js");
-runtime.loadChunk("server/chunks/_d0b7d1._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/type-indicator/route/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/type-indicator/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZA50uFZn8tU6PXxXqkuVeuyc7scuffFd6BvmjCuArbQ=\"\n}"
